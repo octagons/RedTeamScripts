38e9950a356f9b6755c85b6a6add0aacd0aadf22
/dev/tomato <dev-tomato@users.noreply.github.com>
2017-12-05 10:36:05 -0600

    intial commit

---
+++ alert_beacon.py
@@ -0,0 +1,31 @@
+import yaml,requests,argparse
+
+parser = argparse.ArgumentParser(description="Sends a pushover alert on a new beacon")
+parser.add_argument('--host')
+parser.add_argument('--ip')
+parser.add_argument('--os')
+parser.add_argument('--version')
+
+args = parser.parse_args()
+
+# Format expected for config:
+# usertokens:
+#	'uthisisapushovertoken' : 'Bob User'
+#	'uthisisanothertokenok'	: 'Joe Hackerman'
+# apptoken:
+#	'aathisismyapptokenlol'
+try:
+	conf = yaml.safe_load(open('config.yaml'))
+except IOError as e:
+	print(dir(e))
+	quit("Error opening config file. Does it exist?")
+
+for usertoken in conf['usertokens'].keys():
+	headers = {'Content-Type' : 'application/x-www-form-urlencoded'}
+	payload = {'token' : conf['apptoken']}
+	payload.update({'user' : usertoken})
+	payload.update({'title' : 'New beacon reporting in!'})
+	payload.update({'sound' : 'cashregister'})
+	payload.update({'priority' : '1'})
+	payload.update({'message' : 'Hostname %s | Internal IP: %s | OS: %s %s ' % (args.host, args.ip, args.os, args.version)})
+	r = requests.post('https://api.pushover.net/1/messages', headers=headers, data=payload)
